55b9d8d5fc8ad6e0da29b88dd46164249f7e108c
CyberShriver <irakozep18.pi@gmail.com>
2022-03-31 09:39:18 +0100

    Declare methods implementation and interface methods for table MARK

--- VehicleFleetManagment/FleetImp/Mark_Imp.cs
+++ VehicleFleetManagment/FleetImp/Mark_Imp.cs
@@ -1,11 +1,152 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using System.Web.UI.WebControls;
+using VehicleFleetManagment.FleetClass;
+using VehicleFleetManagment.FleetModel;
 
 namespace VehicleFleetManagment.FleetImp
 {
-    public class Mark_Imp
+    public class Mark_Imp:Mark_Interface
     {
+        int msg;
+        MARK M = new MARK();
+
+        //ADD METHOD
+        public int Add(Mark_Class Ma)
+        {
+            using (MINISTRY_DB_Connection con = new MINISTRY_DB_Connection())
+            {
+                M.Mark_Name = Ma.Mark_Name;
+                M.Comment = Ma.Comment;
+
+                con.MARKs.Add(M);
+
+                if (con.SaveChanges() > 0)
+                {
+                    msg = 1;
+                }
+                else
+                {
+                    msg = 0;
+                }
+            }
+
+
+            return msg;
+        }
+
+        //MODIFY METHOD
+        public int Modify(Mark_Class Ma, int id)
+        {
+            using (MINISTRY_DB_Connection con = new MINISTRY_DB_Connection())
+            {
+                MARK M = new MARK();
+                M = con.MARKs.Where(x => x.MARK_ID == id).FirstOrDefault();
+
+                if (M != null)
+                {
+
+                    M.Mark_Name = Ma.Mark_Name;
+                    M.Comment = Ma.Comment;
+
+                    con.MARKs.Add(M);
+
+                    if (con.SaveChanges() > 0)
+                    {
+                        con.MARKs.Add(M);
+                        con.Entry(M).State = EntityState.Modified;
+
+                        msg = 1;
+                    }
+
+                    else
+                        msg = 0;
+                }
+            }
+
+
+            return msg;
+        }
+
+        //DELETE METHOD
+        public int Delete(int id)
+        {
+            using (MINISTRY_DB_Connection con = new MINISTRY_DB_Connection())
+            {
+                var obj = con.MARKs.Where(x => x.MARK_ID == id).First();
+
+                if (con.Entry(obj).State == EntityState.Detached)
+                {
+                    con.MARKs.Attach(obj);
+
+                }
+                con.MARKs.Remove(obj);
+                con.SaveChanges();
+                return msg;
+            }
+
+        }
+
+        //DISPLAY METHOD
+        public void Display(GridView gd)
+        {
+            using (MINISTRY_DB_Connection con = new MINISTRY_DB_Connection())
+            {
+                var obj = (from M in con.MARKs
+
+                           select new
+                           {
+                               MARK_ID = M.MARK_ID,
+                               Mark_Name = M.Mark_Name,
+                               Comment = M.Comment,
+                           }
+                           ).ToList();
+
+                gd.DataSource = obj;
+                gd.DataBind();
+            }
+
+        }
+
+        //PROVIDE METHOD
+        public void provide(Mark_Class Ma, int id)
+        {
+            using (MINISTRY_DB_Connection con = new MINISTRY_DB_Connection())
+            {
+                M = con.MARKs.Where(x => x.MARK_ID == id).FirstOrDefault();
+
+                Ma.Mark_Name = M.Mark_Name;
+                Ma.Comment = M.Comment;
+
+            }
+        }
+
+        //REASEARCH METHOD
+        public void Research(GridView gd, string SearchText)
+        {
+            using (MINISTRY_DB_Connection con = new MINISTRY_DB_Connection())
+            {
+                var obj = (from M in con.MARKs
+                           where
+                      M.Mark_Name == SearchText ||
+                      M.Comment == SearchText
+
+                           select new
+                           {
+                               MARK_ID = M.MARK_ID,
+                               Mark_Name = M.Mark_Name,
+                               Comment = M.Comment,
+
+                           }
+                           ).ToList();
+
+                gd.DataSource = obj;
+                gd.DataBind();
+            }
+
+        }
     }
 }
--- VehicleFleetManagment/FleetImp/Mark_Interface.cs
+++ VehicleFleetManagment/FleetImp/Mark_Interface.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.UI.WebControls;
+using VehicleFleetManagment.FleetClass;
 
 namespace VehicleFleetManagment.FleetImp
 {
     public interface Mark_Interface
     {
+
+        //Add method
+         int Add(Mark_Class Ma);
+
+        //Modify Method
+         int Modify(Mark_Class Ma, int id);
+
+        //Delete Method
+         int Delete(int id);
+
+        //Display Method
+         void Display(GridView gd);
+
+        //Provide Method
+         void provide(Mark_Class Ma, int id);
+
+        //Research Method
+         void Research(GridView gd, string SearchText);
+
     }
 }
